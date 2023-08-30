06e8641d467502b554c4e423f5df2cc12df9a738
jprix <parisijason@gmail.com>
2023-08-30 11:34:24 -0400

    added cookie handling

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,11 +5,17 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import TransferDashboard from '../components/TransfersDashboard';
+import Cookies from 'js-cookie';
+import cookie from 'cookie';
 
-const HomePage = () => {
+
+const HomePage = (props) => {
   const [openMeshModal, setOpenMeshModal] = useState(false);
   const [catalogLink, setCatalogLink] = useState('');
-  const [authData, setAuthData] = useState([]);
+  
+const [existingAuthData, setExistingAuthData] = useState(props.existingAuthData);
+
 
   const getCatalogLink = async () => {
     const link = await fetch('/api/catalog');
@@ -28,8 +34,17 @@ const HomePage = () => {
 
   const handleSuccess = (newAuthData) => {
     console.log('Broker connected successfully:', newAuthData);
-    setAuthData([...authData, newAuthData]);
+    // Combine existing authData array with the new authData object
+    const updatedAuthData = [...existingAuthData, newAuthData];
+    setExistingAuthData(updatedAuthData);
+  
+    // Set the updated authData array to the cookie
+    const maxExpiresInSeconds = Math.max(...updatedAuthData.map(obj => obj.accessToken.expiresInSeconds));
+    const inOneHour = new Date(new Date().getTime() + maxExpiresInSeconds * 1000);
+  
+    Cookies.set('authData', JSON.stringify(updatedAuthData), { expires: inOneHour });
   };
+  
   
 
   const handleExit = (error) => {
@@ -37,16 +52,18 @@ const HomePage = () => {
     // Handle the broker connection closed event
   };
 
-  console.log('this is the authData', authData)
+  console.log('this is the authData', Cookies.get('authData'))
 
   return (
     <div>
       <h1>Mesh Explorer</h1>
-      {authData.length === 0 ?
-      <Button variant="contained" color="primary" onClick={getCatalogLink}>
-        Connect to Mesh
-      </Button> : <Button variant="contained" color="primary" onClick={getCatalogLink}>
-        Link Another Account </Button>
+      {existingAuthData === undefined || existingAuthData.length === 0 ?
+  <Button variant="contained" color="primary" onClick={getCatalogLink}>
+    Connect to Mesh
+  </Button> : 
+  <Button variant="contained" color="primary" onClick={getCatalogLink}>
+    Link Another Account
+  </Button>
 }
 
       {openMeshModal && (
@@ -59,34 +76,37 @@ const HomePage = () => {
         />
       )}
 
-{authData.length > 0 && (
+{existingAuthData.length > 0 && (
   <Grid container spacing={3}>
-    {authData.map((data, index) => (
+    {existingAuthData?.map((data, index) => (
       <Grid item xs={12} key={index}>
         <Card>
           <CardContent>
             <Typography variant="h5" component="div">
-              Broker: {data.accessToken.brokerName}
+              Broker: {data?.accessToken?.brokerName}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Broker Type: {data.accessToken.brokerType}
+                  Broker Type: {data?.accessToken?.brokerType}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Expires In: {data.accessToken.expiresInSeconds} seconds
+                  Expires In: {data?.accessToken?.expiresInSeconds} seconds
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Account Name: {data.accessToken.accountTokens[0].account.accountName}
+                  Account Name: {data?.accessToken?.accountTokens[0].account.accountName}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Fund: {data.accessToken.accountTokens[0].account.fund}
+                  Fund: {data?.accessToken?.accountTokens[0]?.account?.fund}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Cash: {data.accessToken.accountTokens[0].account.cash}
+                  Cash: {data?.accessToken?.accountTokens[0]?.account?.cash}
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
         ))}
+         <Grid item xs={12}>
+      <TransferDashboard />
+    </Grid>
         </Grid>
       )}
     </div>
@@ -94,3 +114,10 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+export async function getServerSideProps(context) {
+  const cookies = cookie.parse(context.req.headers.cookie ?? '');
+  const existingAuthData = cookies.authData ? JSON.parse(cookies.authData) : [];
+  return { props: { existingAuthData } };
+}
