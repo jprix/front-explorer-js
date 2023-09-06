d765d36d73b4a29c3365caf5870da848b22807e1
jprix <parisijason@gmail.com>
2023-09-06 09:08:03 -0400

    prettier fixes

--- pages/api/catalog.jsx
+++ pages/api/catalog.jsx
@@ -1,34 +1,33 @@
 export default async function handler(req, res) {
+  const { PROD_API_KEY, MESH_API_URL, CLIENT_ID } = process.env;
 
-    const { PROD_API_KEY, MESH_API_URL, CLIENT_ID } = process.env;   
-    
-    const { enableTransfers } = req.query;
-console.log(enableTransfers)
-    if (req.method !== 'GET') {
-      return res.status(405).json({ error: 'Method not allowed' });
-    }
-  
-    try {
-      const getCatalogLink = await fetch(
-        `${MESH_API_URL}/api/v1/cataloglink?UserId=${CLIENT_ID}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Client-Id': CLIENT_ID,
-            'X-Client-Secret': PROD_API_KEY
-          },
-        }
-      );
-      if (!getCatalogLink.ok) {
-        const responseBody = await getCatalogLink.json();
-        const errorMessage = `Failed to retrieve or generate catalogLink. Status: ${getCatalogLink.status} - ${getCatalogLink.statusText}. Message: ${responseBody.message}`;
-
-        throw new Error(errorMessage);
-      }
-      const response = await getCatalogLink.json();
-      return res.status(200).json(response);
-    } catch (error) {
-      console.log('this was the error from Mesh', error);
-      res.status(500).json({ error: `Something went wrong: ${error.message}` });
-    }
+  const { enableTransfers } = req.query;
+  console.log(enableTransfers);
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
+
+  try {
+    const getCatalogLink = await fetch(
+      `${MESH_API_URL}/api/v1/cataloglink?UserId=${CLIENT_ID}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Client-Id': CLIENT_ID,
+          'X-Client-Secret': PROD_API_KEY,
+        },
+      }
+    );
+    if (!getCatalogLink.ok) {
+      const responseBody = await getCatalogLink.json();
+      const errorMessage = `Failed to retrieve or generate catalogLink. Status: ${getCatalogLink.status} - ${getCatalogLink.statusText}. Message: ${responseBody.message}`;
+
+      throw new Error(errorMessage);
+    }
+    const response = await getCatalogLink.json();
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log('this was the error from Mesh', error);
+    res.status(500).json({ error: `Something went wrong: ${error.message}` });
+  }
+}
--- pages/api/transfers/execute/index.jsx
+++ pages/api/transfers/execute/index.jsx
@@ -1,41 +1,38 @@
 export default async function handler(req, res) {
-    console.log('*** hit preview ' , req.method, req.body); // log the request method and body
-  
-    const { PROD_API_KEY, MESH_API_URL, CLIENT_ID } = process.env;  
-  
-    const payload = req.body;
-    
-  
-    if (req.method !== 'POST') {
-      return res.status(405).json({ error: 'Method not allowed' });
-    }
-  
-    try {
-      const executeTransfer = await fetch(
-        `${MESH_API_URL}/api/v1/transfers/managed/execute`,
-        {
-          method: 'POST',
-          body: JSON.stringify(payload),
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Client-Id': CLIENT_ID,
-            'X-Client-Secret': PROD_API_KEY
-          },
-        }
-      );
-      if (!executeTransfer.ok) {
-        const responseBody = await executeTransfer.json();
-        console.error('Error response from Mesh API:', responseBody);
-        const errorMessage = `Failed to execute transfer. Status: ${executeTransfer.status} - ${executeTransfer.statusText}. Message: ${responseBody.message}`;
-  
-        throw new Error(
-          `Failed to Execute transfer: ${errorMessage}`
-        );
-      }
-      const response = await executeTransfer.json();
-      return res.status(200).json(response);
-    } catch (error) {
-      console.log('this was the error from Mesh', error);
-      res.status(500).json({ error: `Something went wrong: ${error.message}` });
-    }
+  console.log('*** hit preview ', req.method, req.body); // log the request method and body
+
+  const { PROD_API_KEY, MESH_API_URL, CLIENT_ID } = process.env;
+
+  const payload = req.body;
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
+
+  try {
+    const executeTransfer = await fetch(
+      `${MESH_API_URL}/api/v1/transfers/managed/execute`,
+      {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Client-Id': CLIENT_ID,
+          'X-Client-Secret': PROD_API_KEY,
+        },
+      }
+    );
+    if (!executeTransfer.ok) {
+      const responseBody = await executeTransfer.json();
+      console.error('Error response from Mesh API:', responseBody);
+      const errorMessage = `Failed to execute transfer. Status: ${executeTransfer.status} - ${executeTransfer.statusText}. Message: ${responseBody.message}`;
+
+      throw new Error(`Failed to Execute transfer: ${errorMessage}`);
+    }
+    const response = await executeTransfer.json();
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log('this was the error from Mesh', error);
+    res.status(500).json({ error: `Something went wrong: ${error.message}` });
+  }
+}
